096f2899a5d5a0a6537ededb757674cfe4f1e2a1
Matsuuu <huhta.matias@gmail.com>
2023-06-21 20:54:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { router } from "./routing/router";
+
+router.start();
---
+++ src/views/home-view.js
@@ -0,0 +1,8 @@
+import { html } from "lit"
+
+export const HomeView = () => {
+    return html`
+        <h2>Hello world!</h2>
+        <a href="/foo">Foo</a>
+    `;
+}
