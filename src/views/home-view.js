abc202f872d6a9166d0c71bc7019c62866b9bb10
Matsuuu <huhta.matias@gmail.com>
2023-06-30 14:32:46 +0300

    Put recipe formatter on front page

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,8 +1,9 @@
 import { html } from "lit"
 import "../component/latest-batches.js";
+import "../component/recipe-formatter.js";
 
 export const HomeView = () => {
-    return html`
+    /*return html`
         <h2>Hello world!</h2>
         <a href="/foo">Foo</a>
         <a href="/recipe-formatter">Recipe formatter</a>
@@ -10,5 +11,8 @@ export const HomeView = () => {
             user-id="6PoxdmkWbogHQfBo8g3gz1jJ6Q23"
             api-key="yAw5a7Nz1HvamRSPTtDeUoAXkAHezdwkFx1UHypRC6TlcwRJ8dLPuCspLLazhLw5"
         ></latest-batches>
-    `;
+    `;*/
+    return html`
+        <recipe-formatter></recipe-formatter>
+    `
 }
